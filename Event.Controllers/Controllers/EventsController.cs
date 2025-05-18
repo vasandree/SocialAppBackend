0e9457c232f94f39dc8dddef02be2fc2817aa8a6
Vasandr15 <vasandrey007@gmail.com>
2025-05-19 02:23:01 +0700

    added event commands to controller

--- Event.Controllers/Controllers/EventsController.cs
+++ Event.Controllers/Controllers/EventsController.cs
@@ -1,6 +1,10 @@
+using Event.Contracts.Commands.Event;
+using Event.Contracts.Dtos.Requests;
+using Event.Contracts.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Shared.Extensions.Extensions;
 
 namespace Event.Controllers.Controllers;
 
@@ -19,33 +23,33 @@ public class EventsController : ControllerBase
     [HttpGet]
     public async Task<IActionResult> GetEvents()
     {
-        return Ok();
+        return Ok(await _sender.Send(new GetEventsQuery(User.GetUserId())));
     }
 
     [HttpGet]
     [Route("{id}")]
     public async Task<IActionResult> GetEvent(Guid id)
     {
-        return Ok();
+        return Ok(await _sender.Send(new GetEventQuery(User.GetUserId(), id)));
     }
 
     [HttpPost]
-    public async Task<IActionResult> CreateEvent()
+    public async Task<IActionResult> CreateEvent([FromBody] CreateEventDto eventDto)
     {
-        return Ok();
+        return Ok(await _sender.Send(new CreateEventCommand(User.GetUserId(), eventDto)));
     }
 
     [HttpPut]
     [Route("{id}")]
-    public async Task<IActionResult> UpdateEvent(Guid id)
+    public async Task<IActionResult> UpdateEvent(Guid id, [FromBody] UpdateEventDto eventDto)
     {
-        return Ok();
+        return Ok(await _sender.Send(new UpdateEventCommand(User.GetUserId(), id, eventDto)));
     }
 
     [HttpDelete]
     [Route("{id}")]
     public async Task<IActionResult> DeleteEvent(Guid id)
     {
-        return Ok();
+        return Ok(await _sender.Send(new DeleteEventCommand(User.GetUserId(), id)));
     }
 }
